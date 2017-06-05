a2d0e147d19d3a1b765212a43928e924b73a62ff
yangran <sfyr111@163.com>
2017-06-05 22:41:44 +0800

    分离滚动组件，第一步实现滚动

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,50 +1,37 @@
 <template>
   <div class="recommend" ref="recommend">
-   <!-- <scroll ref="scroll" class="recommend-content" :data="">
+    <scroll class="recommend-content" :data="discList">
       <div>
-        <div class="slider-warpper" v-if="" ref="sliderWrapper">
+        <div v-if="recommends.length" class="slider-wrapper">
           <slider>
-            <div v-for="">
-              <a href="">
-                <img class="needsclick" src="" alt="">
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl">
               </a>
             </div>
           </slider>
         </div>
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
-    </scroll> -->
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="item in discList" class="item">
-            <div class="icon">
-              <img width="60" height="60" :src="item.imgurl" alt="">
-            </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+  import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
   import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
@@ -77,7 +64,8 @@
       }
     },
     components: {
-      Slider
+      Slider,
+      Scroll
     }
   }
 </script>
