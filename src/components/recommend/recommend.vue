3b856f765528ac65895ad20a995670e64be0a2c6
yangran <sfyr111@163.com>
2017-06-05 22:08:48 +0800

    完成轮播和数据抓取

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="recommend" ref="recommend">
+   <!-- <scroll ref="scroll" class="recommend-content" :data="">
+      <div>
+        <div class="slider-warpper" v-if="" ref="sliderWrapper">
+          <slider>
+            <div v-for="">
+              <a href="">
+                <img class="needsclick" src="" alt="">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+        </div>
+      </div>
+    </scroll> -->
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+          <li v-for="item in discList" class="item">
+            <div class="icon">
+              <img width="60" height="60" :src="item.imgurl" alt="">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Slider from 'base/slider/slider'
+  import { getRecommend, getDiscList } from 'api/recommend'
+  import { ERR_OK } from 'api/config'
+  export default {
+    name: 'recommend',
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      }
+    },
+    created() {
+      this._getRecommend()
+      this._getDiscList()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then(res => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList() {
+        getDiscList().then(res => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+          }
+        })
+      }
+    },
+    components: {
+      Slider
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~common/scss/variable";
+  .recommend {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    .recommend-content {
+      height: 100%;
+      overflow: hidden;
+      .slider-wrapper {
+        position: relative;
+        width: 100%;
+        overflow: hidden;
+      }
+      .recommend-list  {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
+          font-size: $font-size-medium;
+          color: $color-theme;
+        }
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
+          }
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: $font-size-medium;
+            .name {
+              margin-bottom: 10px;
+              color: $color-text;
+            }
+            .desc {
+              color: $color-text-d;
+            }
+          }
+        }
+        .loading-container {
+          position: absolute;
+          width: 1005;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+    }
+  }
+</style>
