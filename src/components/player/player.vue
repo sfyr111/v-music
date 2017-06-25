d113c3b1972a4f7382b5e2f369d1ff6df60f90df
yangran <sfyr111@163.com>
2017-06-26 04:14:32 +0800

    歌词滚动列表实现

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -24,6 +24,16 @@
             </div>
           </div>
         </div>
+        <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
+          <div class="lyric-wrapper">
+            <div v-if="currentLyric">
+              <p class="text"
+                 :class="{'current': currentLineNum === index}"
+                 ref="lyricLine"
+                 v-for="(line, index) in currentLyric.lines">{{line.txt}}</p>
+            </div>
+          </div>
+        </scroll>
       </div>
       <div class="bottom">
         <div class="progress-wrapper">
@@ -88,6 +98,7 @@
   import { playMode } from 'common/js/config'
   import { shuffle } from 'common/js/util'
   import Lyric from 'lyric-parser'
+  import Scroll from 'base/scroll/scroll'
 
   const transform = prefixStyle('transform')
 
@@ -98,7 +109,8 @@
         songReady: false,
         currentTime: 0,
         radius: 32,
-        currentLyric: null
+        currentLyric: null,
+        currentLineNum: 0
       }
     },
     computed: {
@@ -262,9 +274,21 @@
       },
       getLyric() {
         this.currentSong.getLyric().then(lyric => {
-          this.currentLyric = new Lyric(lyric)
+          this.currentLyric = new Lyric(lyric, this.handleLyric)
+          if (this.playing) {
+            this.currentLyric.play()
+          }
           console.log(this.currentLyric)
         })
+      },
+      handleLyric({ lineNum, txt }) {
+        this.currentLineNum = lineNum
+        if (lineNum > 5) {
+          let lineEl = this.$refs.lyricLine[lineNum - 5]
+          this.$refs.lyricList.scrollToElement(lineEl, 1000)
+        } else {
+          this.$refs.lyricList.scrollTo(0, 0, 1000)
+        }
       },
       _pad(num, n = 2) {
         let len = num.toString().length
@@ -316,7 +340,8 @@
     },
     components: {
       progressBar,
-      progressCircle
+      progressCircle,
+      Scroll
     }
   }
 </script>
